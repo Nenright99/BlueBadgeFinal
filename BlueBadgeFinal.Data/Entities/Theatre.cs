83f1d8f0478d75e82da752ee7376fdbfa60ba0ec
Nicholas Enright <NEnright1999@gmail.com>
2021-07-27 12:26:44 -0400

    Movie relatinship created

--- BlueBadgeFinal.Data/Entities/Theatre.cs
+++ BlueBadgeFinal.Data/Entities/Theatre.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,8 @@ namespace BlueBadgeFinal.Data.Entities
         [Required]
         public string Name { get; set; }
         public string Address { get; set; }
+        [ForeignKey(nameof(Movie))]
+        public virtual Movie MovieAddress { get; set; }
         public ICollection<Movie> MovieList { get; set; }
         public ICollection<Review> ReviewList { get; set; }
     }
